f33dac92ccf9aa2ae89a312efd3ae2f1f8f36b63
f-rahim <freelancerrahim26@gmail.com>
2023-08-28 11:45:22 +0600

    Fix admin link active color logig

--- src/components/AdminLink/AdminLink.jsx
+++ src/components/AdminLink/AdminLink.jsx
@@ -1,26 +1,16 @@
 /* eslint-disable react/prop-types */
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import * as fa from "react-icons/fa";
-import style from "./AdminLink.module.css";
+import "./AdminLink.css";
 
 const AdminLink = ({ children, icon, to }) => {
   const Icon = fa[icon];
   const NavIcon = Icon || fa["FaAtlassian"];
 
-  // Check the link is active or not
-  const location = useLocation();
-  const isActive = () => {
-    const pathname = location.pathname;
-    console.log(to, pathname);
-    return to === pathname; // return true or false
-  };
-
   return (
     <NavLink
       to={to}
-      className={`flex items-center gap-2 py-2 font-semibold ${
-        isActive() && style.activeNav
-      }`}
+      className={`flex items-center gap-2 py-2 font-semibold admin-link`}
     >
       <NavIcon className="text-xl" />
       {children}
