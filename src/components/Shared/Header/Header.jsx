dd1c1103d2319bda707a228931cb1175b2cb40d9
f-rahim <freelancerrahim26@gmail.com>
2023-08-31 11:36:04 +0600

    Update header menu / Added my account route in frontend

--- src/components/Shared/Header/Header.jsx
+++ src/components/Shared/Header/Header.jsx
@@ -1,8 +1,10 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
 import logo from "./logo-light.png";
+import useAuthContex from "../../../hooks/useAuthContex";
 
 const Header = () => {
+  const { user } = useAuthContex();
   return (
     <header className="header-area pt-10 pb-4 absolute top-0 z-10 w-full">
       <div className="container">
@@ -16,16 +18,29 @@ const Header = () => {
 
             <div className="menu-area">
               <ul className="flex gap-2 text-white">
-                <li>
-                  <Link to="/login" className="py-4 px-6">
-                    Login
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/signup" className="py-4 px-6">
-                    Sign Up
-                  </Link>
-                </li>
+                {user?.uid ? (
+                  <li>
+                    <Link
+                      to="/dashboard/"
+                      className="py-3 rounded px-6 btn-primary"
+                    >
+                      My Account
+                    </Link>
+                  </li>
+                ) : (
+                  <>
+                    <li>
+                      <Link to="/login" className="py-3 rounded px-6">
+                        Login
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/signup" className="py-3 rounded px-6">
+                        Sign Up
+                      </Link>
+                    </li>
+                  </>
+                )}
               </ul>
             </div>
           </div>
